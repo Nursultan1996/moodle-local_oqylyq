54da1f32d4e71fe5e16580da3e46e3081c197335
Nursultan1996 <85542749+Nursultan1996@users.noreply.github.com>
2025-12-11 14:21:05 +0500

    Update and rename .icommand.php to icommand.php

--- classes/local/icommand.php
+++ classes/local/icommand.php
@@ -23,7 +23,7 @@
   * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
   */
 
-namespace quizaccess_oqylyq\oqylyq;
+namespace quizaccess_oqylyq\local;
 
 interface icommand {
     public function getRequestUrl() : string;
